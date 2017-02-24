b644c74af583f34eae1b1b0645d6141ce65d9594
devsmt <devsmt@gmail.com>
2017-02-24 15:17:36 +0100

    wrkst updt

--- _jedit/templates/GO/hello.go
+++ _jedit/templates/GO/hello.go
@@ -1,1 +1,17 @@
+/*
+scopo del programma:
+*/
+// compilation:
+//  GOOS=windows GOARCH=386 go build -o virgo.exe main.go
+//  go build main.go
 
+package main
+
+import (
+    "fmt"
+)
+
+func init(){}
+func main() {
+    fmt.Println("Hello, go")
+}
